93d0d42ce2a7f190e94214313a10b478b25f7d1f
Jonathan Bowman <jbowmancargas@users.noreply.github.com>
2022-03-14 10:14:14 -0400

    feat: tidied up package lists, fixed package/repo paths

--- workstation-playbook.py
+++ workstation-playbook.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Script for full Linux graphical workstation."""
+from pathlib import Path
+
 import shaper.dnf
 import shaper.dotfiles
 import shaper.download
@@ -9,17 +11,19 @@ import shaper.localpy
 import shaper.npm
 import shaper.rust
 
+BASE = Path.home() / "shaper"
+
 
 def run() -> None:
     """Run commands in order."""
-    shaper.dnf.install_rpm_keys("packages/rpm_keys.json")
-    shaper.dnf.install_dnf_repos("repos/workstation_repos.txt")
-    shaper.dnf.install_copr_repos("repos/workstation_copr_repos.txt")
+    shaper.dnf.install_rpm_keys(f"{BASE}/packages/rpm_keys.json")
+    shaper.dnf.install_dnf_repos(f"{BASE}/repos/workstation_repos.txt")
+    shaper.dnf.install_copr_repos(f"{BASE}/repos/workstation_copr_repos.txt")
     shaper.dnf.install_rpmfusion()
-    shaper.dnf.install_dnf_packages("packages/base_dnf.txt")
-    shaper.dnf.install_dnf_packages("packages/workstation_dnf.txt")
-    shaper.localpy.install_pip_packages("packages/base_pip.txt")
-    shaper.fonts.install_fonts("fonts/workstation-fonts.txt")
+    shaper.dnf.install_dnf_packages(f"{BASE}/packages/base_dnf.txt")
+    shaper.dnf.install_dnf_packages(f"{BASE}/packages/workstation_dnf.txt")
+    shaper.localpy.install_pip_packages(f"{BASE}/packages/base_pip.txt")
+    shaper.fonts.install_fonts(f"{BASE}/fonts/workstation-fonts.txt")
     shaper.download.install_with_remote_script(
         "rustup", "https://sh.rustup.rs", ["-y", "--no-modify-path"]
     )
@@ -32,9 +36,9 @@ def run() -> None:
     shaper.dotfiles.dotfile_git_restore(
         "workstation", "git@github.com:bowmanjd/dotfiles-workstation.git"
     )
-    shaper.npm.install_npm_packages("packages/base_npm.txt")
-    shaper.golang.install_go_packages("packages/base_go.txt")
-    shaper.rust.install_rust_packages("packages/base_rust.txt")
+    shaper.npm.install_npm_packages(f"{BASE}/packages/base_npm.txt")
+    shaper.golang.install_go_packages(f"{BASE}/packages/base_go.txt")
+    shaper.rust.install_rust_packages(f"{BASE}/packages/base_rust.txt")
 
 
 if __name__ == "__main__":
