787ea156e7ec946e468aa564b5d227d2d0927e43
Jonathan Bowman <git@bowmanjd.com>
2022-03-14 07:26:01 -0400

    feat: exposed go updater command line script

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import setuptools
 setuptools.setup(
     author="Jonathan Bowman",
     description="Configure a vanilla Fedora system.",
+    entry_points={"console_scripts": ["update-go=shaper.golang:go_update"]},
     name="shaper",
     py_modules=["shaper"],
     version="0.1.0",
